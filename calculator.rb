96182fbcdf80dea58ff0ea50172e663394fed362
boklos <boklos@github.com>
2016-04-21 19:47:00 -0400

    calculator

--- calculator.rb
+++ calculator.rb
@@ -3,33 +3,109 @@
 # perform the operation
 # display the resuly
 
-Kernel.puts(" Welcome to calculator")
+def prompt(message)
+  puts("=>#{message}")
+end
+def number_valid(num)
+  num.to_i != 0
+end
+def operation_message(op)
+  case op
+  when '1'
+    'Adding now...'
+  when '2'
+    'Substracting now...'
+  when '3'
+    'Multiplying now...'
+  when '4'
+    'Dividing now...'
+  end
+end
 
-Kernel.puts(" pick the first number ")
-num1 = Kernel.gets().chomp()
 
-Kernel.puts("pick the second number")
+prompt(" Welcome to calculator Please enter your name")
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt(" please enter your name")
+  else
+    break
+  end
 
-num2 = Kernel.gets().chomp()
+end
 
-Kernel.puts (" choose your operator 1) addition 2)substraction 3) multiply 4) divide")
 
-operator = Kernel.gets().chomp()
+prompt("Hello #{name}")
+loop do #main loop
 
-if operator == "1"
+num1 = ''
+loop do
+  prompt(" pick the first number ")
+  num1 = Kernel.gets().chomp()
+
+  if number_valid(num1)
+    break
+  else
+    prompt("please enter a valid number")
+  end
+end
+
+num2 = ''
+loop do
+  prompt("pick the second number")
+  num2 = Kernel.gets().chomp()
+  if number_valid(num2)
+    break
+  else
+    prompt("please enter a valid number")
+  end
+end
+
+prompt_list = <<-LIST
+What operation do you want to perform?
+1) Addition
+2) Substract
+3) Multiply
+4) Divide
+
+LIST
+  prompt(prompt_list)
+
+operator = ''
+loop do
+  operator = Kernel.gets().chomp()
+  if %w(1 2 3 4 ).include?(operator)
+    break
+  else
+    prompt("please choose one of the 4 operations only")
+  end
+end
+
+
+prompt("#{operation_message(operator)} #{name}")
+
+case operator
+when '1'
   result = num1.to_i + num2.to_i
-elsif
-  operator == "2"
+when '2'
   result = num1.to_i- num2.to_i
-elsif
-    operator == "3"
-    result = num1.to_i * num2.to_i
-else
-   result = num1.to_f / num2.to_f
+when '3'
+  result = num1.to_i * num2.to_i
+when '4'
+  result = num1.to_f / num2.to_f
+end
+
+
+
+prompt(" the result is #{result}")
+
+prompt("do you have another calculation? type Y for yes")
+answer = gets().chomp()
+break unless answer.downcase().start_with?('y')
+
 end
 
 
 
 
-
-Kernel.puts(" the result is #{result}")
