d61a4b2fa3b398794498bd9065003d67342b6678
boklos <boklos@github.com>
2016-04-21 20:12:39 -0400

    calculator with rubocop

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,11 @@
-# ask user for two numbers
-# ask user for an operation to perform
-# perform the operation
-# display the resuly
-
 def prompt(message)
   puts("=>#{message}")
 end
+
 def number_valid(num)
   num.to_i != 0
 end
+
 def operation_message(op)
   case op
   when '1'
@@ -22,7 +19,6 @@ def operation_message(op)
   end
 end
 
-
 prompt(" Welcome to calculator Please enter your name")
 name = ''
 loop do
@@ -32,80 +28,67 @@ loop do
   else
     break
   end
-
 end
-
 
 prompt("Hello #{name}")
-loop do #main loop
+loop do # main loop
+  num1 = ''
+  loop do
+    prompt(" pick the first number ")
+    num1 = Kernel.gets().chomp()
 
-num1 = ''
-loop do
-  prompt(" pick the first number ")
-  num1 = Kernel.gets().chomp()
-
-  if number_valid(num1)
-    break
-  else
-    prompt("please enter a valid number")
+    if number_valid(num1)
+      break
+    else
+      prompt("please enter a valid number")
+    end
   end
-end
 
-num2 = ''
-loop do
-  prompt("pick the second number")
-  num2 = Kernel.gets().chomp()
-  if number_valid(num2)
-    break
-  else
-    prompt("please enter a valid number")
+  num2 = ''
+  loop do
+    prompt("pick the second number")
+    num2 = Kernel.gets().chomp()
+    if number_valid(num2)
+      break
+    else
+      prompt("please enter a valid number")
+    end
   end
-end
 
-prompt_list = <<-LIST
-What operation do you want to perform?
-1) Addition
-2) Substract
-3) Multiply
-4) Divide
+  prompt_list = <<-LIST
+  What operation do you want to perform?
+  1) Addition
+  2) Substract
+  3) Multiply
+  4) Divide
 
-LIST
+  LIST
   prompt(prompt_list)
 
-operator = ''
-loop do
-  operator = Kernel.gets().chomp()
-  if %w(1 2 3 4 ).include?(operator)
-    break
-  else
-    prompt("please choose one of the 4 operations only")
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+    if %w(1 2 3 4 ).include?(operator)
+      break
+    else
+      prompt("please choose one of the 4 operations only")
+    end
   end
+  prompt("#{operation_message(operator)} #{name}")
+
+  case operator
+  when '1'
+    result = num1.to_i + num2.to_i
+  when '2'
+    result = num1.to_i - num2.to_i
+  when '3'
+    result = num1.to_i * num2.to_i
+  when '4'
+    result = num1.to_f / num2.to_f
+  end
+
+  prompt(" the result is #{result}")
+  prompt("do you have another calculation? type Y for yes")
+  answer = gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
-
-
-prompt("#{operation_message(operator)} #{name}")
-
-case operator
-when '1'
-  result = num1.to_i + num2.to_i
-when '2'
-  result = num1.to_i- num2.to_i
-when '3'
-  result = num1.to_i * num2.to_i
-when '4'
-  result = num1.to_f / num2.to_f
-end
-
-
-
-prompt(" the result is #{result}")
-
-prompt("do you have another calculation? type Y for yes")
-answer = gets().chomp()
-break unless answer.downcase().start_with?('y')
-
-end
-
-
-
-
